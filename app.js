5d667b7a4782f5d674a3bf1cf80f11e12109b4de
keinsize <118171210+keinsize@users.noreply.github.com>
2023-02-06 10:15:55 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+const upBtn = document.querySelector('.up-button');
+const downBtn = document.querySelector('.down-button');
+const sideBar = document.querySelector('.sidebar');
+const mainSlide = document.querySelector('.main-slide');
+const slidesCount = mainSlide.querySelectorAll('div').length;
+const container = document.querySelector('.container');
+
+sideBar.style.top = `-${(slidesCount - 1) * 100}vh`;
+
+let activeSlide = 0;
+
+upBtn.addEventListener('click', () => {
+    changeSlide('up');
+});
+
+downBtn.addEventListener('click', () => {
+    changeSlide('down');
+});
+
+function changeSlide(direction) {
+    if (direction === 'up') {
+        activeSlide++;
+        if (activeSlide === slidesCount) {
+            activeSlide = 0;
+        }
+    } else if (direction === 'down') {
+        activeSlide--;
+        if (activeSlide < 0) {
+            activeSlide = slidesCount - 1;
+        }
+    }
+
+    const height = container.clientHeight;
+
+    mainSlide.style.transform = `translateY(-${activeSlide * height}px)`;
+    sideBar.style.transform = `translateY(${activeSlide * height}px)`;
+}
